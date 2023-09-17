488107c4d26c950c13ab609657c070853c5a8a2f
nhnserg <nhnsergfr@gmail.com>
2023-09-17 10:54:43 +0100

    now it's good

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+function saveToLocalStorage() {
+  const emailInput = document.querySelector('input[name="email"]');
+  const messageInput = document.querySelector('textarea[name="message"]');
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function fillFormFromLocalStorage() {
+  const formData =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  const emailInput = document.querySelector('input[name="email"]');
+  const messageInput = document.querySelector('textarea[name="message"]');
+
+  emailInput.value = formData.email || '';
+  messageInput.value = formData.message || '';
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = document.querySelector('input[name="email"]');
+  const messageInput = document.querySelector('textarea[name="message"]');
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(saveToLocalStorage, 500));
+
+window.addEventListener('load', fillFormFromLocalStorage);
+
+form.addEventListener('submit', handleSubmit);
