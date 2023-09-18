20d6410228b6909bd20a21bfeab0464bfe9e8181
nhnserg <nhnsergfr@gmail.com>
2023-09-18 14:48:33 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,15 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+// глобальний об'єет ФормДата
+let formData = {};
 
-let formData = {}; // глобальний об'єет ФормДата
 function saveToLocalStorage(e) {
-  // универсальний скрипт за допомогою якого ми зберігаємо данні. динамічно додаємо в обьект потім цей об'єкт зберігаємо
+  // 1.1
   formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-// зберігаєтся всі дочирні елементи форми таким чином ми можемо динамічно до них звертатись, звертатись до значення велью, задавати велью і таке отримувати при деструктурезації. буде працювати з будьякою формою і кількістю полів.
+// 2.1
 function fillFormFromLocalStorage() {
   try {
     const savedData = localStorage.getItem(STORAGE_KEY);
@@ -37,3 +38,6 @@ form.addEventListener('input', throttle(saveToLocalStorage, 500));
 window.addEventListener('load', fillFormFromLocalStorage);
 
 form.addEventListener('submit', handleSubmit);
+
+//1.1 универсальний скрипт за допомогою якого ми зберігаємо данні. динамічно додаємо в обьект потім цей об'єкт зберігаємо
+//2.1 зберігаєтся всі дочирні елементи форми таким чином ми можемо динамічно до них звертатись, звертатись до значення велью, задавати велью і таке отримувати при деструктурезації. буде працювати з будьякою формою і кількістю полів.
