dc0a0877f627b07c45292eaa4759b214d073f3bf
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-03 20:23:05 +0100

    Update 03-feedback.js
    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -39,3 +39,39 @@ window.addEventListener('load', fillFormFromLocalStorage);
 formEl.addEventListener('submit', handleSubmit);
 //1.1 универсальний скрипт за допомогою якого ми зберігаємо данні. динамічно додаємо в обьект потім цей об'єкт зберігаємо
 //2.1 зберігаєтся всі дочирні елементи форми таким чином ми можемо динамічно до них звертатись, звертатись до значення велью, задавати велью і таке отримувати при деструктурезації. буде працювати з будьякою формою і кількістю полів.
+
+// Version 2
+
+import throttle from 'lodash.throttle';
+
+const localStorageKey = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const { email, message } = feedbackForm.elements;
+
+const loadFormState = () => {
+  const savedState = JSON.parse(localStorage.getItem(localStorageKey));
+  if (savedState) {
+    email.value = savedState.email;
+    message.value = savedState.message;
+  }
+};
+
+const saveFormState = throttle(() => {
+  localStorage.setItem(
+    localStorageKey,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}, 500);
+
+[...feedbackForm.querySelectorAll('input, textarea')].forEach(inputField => {
+  inputField.addEventListener('input', saveFormState);
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  feedbackForm.reset();
+  localStorage.removeItem(localStorageKey);
+});
+
+window.addEventListener('load', loadFormState);
